af9cab34a743357230e862c17bffaff687dbd435
Juan David Duque <jduque0289@gmail.com>
2023-10-09 16:35:57 -0500

    Challenge 1: FizzBuzz solution

---
+++ reto1.py
@@ -0,0 +1,29 @@
+# /*
+#  * Escribe un programa que muestre por consola (con un print) los
+#  * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+#  * cada impresión), sustituyendo los siguientes:
+#  * - Múltiplos de 3 por la palabra "fizz".
+#  * - Múltiplos de 5 por la palabra "buzz".
+#  * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+#  */
+
+# num = list(range(1, 101))
+# print(num)
+
+
+# for i in range(1, 101):
+# Check multile of 3
+def fizz_buzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+
+    else:
+        return num
+
+
+for n in range(1, 100):
+    print(fizz_buzz(n))
